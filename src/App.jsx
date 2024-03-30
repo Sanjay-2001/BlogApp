959ac472f4ca3d185ee0e9347d42181f07b71e9b
SanjayDutta <sanjaydutta976.com>
2024-03-30 10:44:58 +0530

    appwrite env setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Blog App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
