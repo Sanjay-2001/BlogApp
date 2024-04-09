650911a9b01922c8ed0dafd55b7328f6d0cafe23
SanjayDutta <sanjaydutta976.com>
2024-04-09 12:11:12 +0530

    config js updated

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -76,19 +76,45 @@ export class Service {
     }
   }
 
-  async getPosts(queries= [Query.equal('status','active')]){
-    try{
-return await this.databases.listDocuments(
-conf.appwriteDatabaseId,conf.appwriteCollectionId,
-queries,
-)
-
-    }catch(error){
-        console.log("Appwrite service :: getPosts :: error ", error);
-return false
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPosts :: error ", error);
+      return false;
     }
   }
 
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite service :: uploadFile :: error ", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error ", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
 const service = new Service();
