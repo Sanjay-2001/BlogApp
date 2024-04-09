1248006802981c17016befef6a0cdfd0d6fcb459
SanjayDutta <sanjaydutta976.com>
2024-04-09 16:22:21 +0530

    redux tool kit configured and header footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Footer, Header } from "./components";
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (
     <>
-      <h1>Blog App</h1>
+      <div className="main">
+        <div className="body">
+          <Header />
+          <main>
+            {/* <Outlet/> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  );
+  ) : null;
 }
 
 export default App;
